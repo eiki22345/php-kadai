0fcc844622baa8bc8c33432ca129ce8d2fee02ec
matsumori <eiki22345@gmail.com>
2025-07-15 18:22:06 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+  <title>PHP連想配列</title>
+  <meta charset="UTF-8">
+  </head>
+  <body>
+    <p>
+      <?php
+      $ingredients = ['name' => 'onion','price' => 200,'weight' => 160];
+      print_r($ingredients);
+      ?>
+
+    </p>
+  </body>
+</html>

df47d9d75466c2f25bdd4348eec036731536fda6
matsumori <eiki22345@gmail.com>
2025-08-26 11:02:43 +0900

    Add files via upload

---
+++ kadai_016/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>書籍管理アプリ</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+  </head>
+
+  <body>
+    <header>
+      <nav>
+        <a href="index.php">書籍管理アプリ</a>
+      </nav>
+    </header>
+    <main>
+      <article class="home">
+        <h1>書籍管理アプリ</h1>
+        <p>『PHPとデータベースを連携しよう』</p>
+        <a href="read.php" class="btn">書籍一覧</a>
+      </article>
+    </main>
+    <footer>
+      <p class="copyright">&copy; 書籍管理アプリ ALL rights reserved.</p>
+    </footer>
+  </body>
+</html>
---
+++ kadai_016/update.php
@@ -0,0 +1,143 @@
+<?php 
+
+$dsn = 'mysql:dbname=php_book_app;host=localhost;charset=utf8mb4';
+$user = 'root';
+$password = '';
+
+if (isset($_POST['submit'])) {
+  try {
+    $pdo = new PDO($dsn,$user,$password);
+
+    $sql_update = '
+    UPDATE books
+    SET book_code = :book_code,
+    book_name = :book_name,
+    price = :price,
+    stock_quantity = :stock_quantity,
+    genre_code = :genre_code
+    WHERE id = :id
+    ';
+
+    $stmt_update = $pdo->prepare($sql_update);
+
+    $stmt_update->bindValue(':book_code',$_POST['book_code']);
+    $stmt_update->bindValue(':book_name',$_POST['book_name']);
+    $stmt_update->bindValue(':price',$_POST['price']);
+    $stmt_update->bindValue(':stock_quantity',$_POST['stock_quantity']);
+    $stmt_update->bindValue(':genre_code',$_POST['genre_code']);
+    $stmt_update->bindValue(':id',$_GET['id']);
+
+    $stmt_update->execute();
+
+    $count = $stmt_update->rowcount();
+
+    $message = "商品を{$count}件更新しました。";
+
+    header("Location: read.php?message={$message}");
+
+
+  } catch(PDOException $e) {
+    exit($e->getMessage());
+  }
+}
+
+if (isset($_GET['id'])) {
+  try {
+    $pdo = new PDO($dsn,$user,$password);
+
+    $sql_select_book = 'SELECT * FROM books WHERE id = :id';
+
+    $stmt_select_book = $pdo->prepare($sql_select_book);
+
+    $stmt_select_book->bindValue(':id',$_GET['id'],PDO::PARAM_INT);
+
+    $stmt_select_book->execute();
+
+    $books = $stmt_select_book->fetch(PDO::FETCH_ASSOC);
+
+    if($books === FALSE) {
+      exit('idパラメータの値が不正です。');
+    }
+
+  
+
+  $sql_select_genre_codes = 'SELECT genre_code FROM books';
+
+  $stmt_select_genre_codes = $pdo->query($sql_select_genre_codes);
+
+  $genre_codes = $stmt_select_genre_codes->fetchAll(PDO::FETCH_COLUMN);
+
+    
+  }  catch (PDOException $e) {
+    exit($e->getMessage());
+  }
+}
+
+try {
+
+} catch(PDOException $e) {
+  exit($e->getMessage());
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>書籍登録</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+  </head>
+
+  <body>
+    <header>
+      <nav>
+        <a href="index.php">書籍管理アプリ</a>
+      </nav>
+    </header>
+    <main>
+      <article class="registration">
+        <h1>書籍編集</h1>
+        <div class="back">
+          <a href="read.php" class="btn">&lt; 戻る</a>
+        </div>
+        <div>
+        <form action="update.php?id=<?= $_GET['id'] ?>" method="post" class="registration-form">
+          <div>          
+            <label for="book_code">書籍コード</label>
+            <input type="number" id="book_code" name="book_code" min="0" max="10000000000" required value="<?= $books['book_code'] ?>">
+            
+            <label for="book_name">書籍名</label>
+            <input type="text" id="book_name" name="book_name" maxlength="50" required value="<?= $books['book_name'] ?>">
+
+            <label for="price">単価</label>
+            <input type="number" id="price" name="price" min="0" max="10000000000" required value="<?= $books['price'] ?>">
+
+            <label for="stock_quantity">在庫数</label>
+            <input type="number" id="stock_quantity" name="stock_quantity" min="0" max="1000000000" required value="<?= $books['stock_quantity'] ?>">
+            
+            <label for="genre_code">ジャンルコード</label>
+            <select id="genre_code" name="genre_code" required>
+              <option disabled selected value>選択してください</option>
+              <?php
+
+              foreach($genre_codes as $genre_code) {
+                echo "<option vlaue='{$genre_code}'>{$genre_code}</option>";
+              }
+              ?>
+            </select>
+          </div>
+          <button type="submit" class="submit-btn" name="submit" value="create">更新</button>
+        </form>
+      </article>
+    </main>
+    <footer>
+      <p class="copyright">&copy; 書籍管理アプリ ALL rights reserved.</p>
+    </footer>
+  </body>
+</html>
